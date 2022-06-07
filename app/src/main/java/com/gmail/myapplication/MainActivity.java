d4cc66f531775656c7c8559e71de1da10983e2a7
James <woghd8503@naver.com>
2022-06-07 22:30:12 +0900

    first App

--- app/src/main/java/com/gmail/myapplication/MainActivity.java
+++ app/src/main/java/com/gmail/myapplication/MainActivity.java
@@ -4,10 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.appCompatActivity;
+import android.view.KeyEvent;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
