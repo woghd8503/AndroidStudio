eb95fc03b0f343b77362edaf0dab524584b72902
James <woghd8503@naver.com>
2022-06-07 22:26:05 +0900

    first App

--- app/src/main/java/com/gmail/myapplication/MainActivity.java
+++ app/src/main/java/com/gmail/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.gmail.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 
 import android.os.Bundle;
 
